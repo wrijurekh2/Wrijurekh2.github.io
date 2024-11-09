8952e06d68c85ded1fb36e14460fa457ce8f93d0
Chek1111 <vqv502@york.ac.uk>
2024-11-09 23:43:48 +0000

    Map changes
    Added a tilemap, at the start of render when you touch the screen it tells you the x,y coordinates according to the tilemap coordinates

---
+++ UniSim/assets/aaa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="aaa" tilewidth="32" tileheight="32" tilecount="13493" columns="131">
+ <image source="MAPV4.png" width="4222" height="3315"/>
+</tileset>
